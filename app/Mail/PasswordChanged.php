4ba8ae09e87881bc70481165b139e371a9a9e086
Aron-tech <aron.papp2003@gmail.com>
2024-11-21 15:23:20 +0100

    Settings email
    When you change your password then the system sends email to you.

---
+++ app/Mail/PasswordChanged.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Container\Attributes\Auth;
+
+class PasswordChanged extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    public function build()
+    {
+        return $this->subject('Jelszó változtatás történt')
+                    ->view('email.changed-password', ['user' => $this->user]);
+    }
+}
